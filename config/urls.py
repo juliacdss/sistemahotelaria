5c0b51687658d15f78563aff56fe49a5a32a0705
juliacdss <juliacostaifc@gmail.com>
2025-06-26 09:11:59 -0300

    feat: urls

--- config/urls.py
+++ config/urls.py
@@ -1,36 +1,22 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import Endereco, Hospede, Quarto, Reserva, Servico, ReservaServico, Pagamento, Funcionario
-from .serializers import EnderecoSerializer, HospedeSerializer, QuartoSerializer, ReservaSerializer, ServicoSerializer, ReservaServicoSerializer, PagamentoSerializer, FuncionarioSerializer
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from hotel.views import (
+    EnderecoViewSet, HospedeViewSet, QuartoViewSet, ReservaViewSet,
+    ServicoViewSet, ReservaServicoViewSet, PagamentoViewSet, FuncionarioViewSet
+)
 
-class EnderecoViewSet(ModelViewSet):
-    queryset = Endereco.objects.all()
-    serializer_class = EnderecoSerializer
+router = DefaultRouter()
+router.register(r'enderecos', EnderecoViewSet)
+router.register(r'hospedes', HospedeViewSet)
+router.register(r'quartos', QuartoViewSet)
+router.register(r'reservas', ReservaViewSet)
+router.register(r'servicos', ServicoViewSet)
+router.register(r'reservaservicos', ReservaServicoViewSet)
+router.register(r'pagamentos', PagamentoViewSet)
+router.register(r'funcionarios', FuncionarioViewSet)
 
-class HospedeViewSet(ModelViewSet):
-    queryset = Hospede.objects.all()
-    serializer_class = HospedeSerializer    
-
-class QuartoViewSet(ModelViewSet):
-    queryset = Quarto.objects.all()
-    serializer_class = QuartoSerializer    
-
-class ReservaViewSet(ModelViewSet):
-    queryset = Reserva.objects.all()
-    serializer_class = ReservaSerializer    
-
-class ServicoViewSet(ModelViewSet):
-    queryset = Servico.objects.all()
-    serializer_class = ServicoSerializer
-
-
-class ReservaServicoViewSet(ModelViewSet):
-    queryset = ReservaServico.objects.all()
-    serializer_class = ReservaServicoSerializer
-
-class PagamentoViewSet(ModelViewSet):
-    queryset = Pagamento.objects.all()
-    serializer_class = PagamentoSerializer    
-
-class FuncionarioViewSet(ModelViewSet):
-    queryset = Funcionario.objects.all()
-    serializer_class = FuncionarioSerializer    
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('admin/', admin.site.urls),
+]
